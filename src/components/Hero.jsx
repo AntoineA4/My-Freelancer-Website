c4f925f71a2d0860ce1a85dc84e999cf0779003c
Antoine Gadrat <antoinegadrat@hotmail.fr>
2026-05-22 17:37:58 +0900

    feat: start hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+function Hero() {
+    return(
+        <section>
+            <div>
+                <h1>
+                    Je crée des sites Web <br />
+                    <em>qui convertissent</em>
+                </h1>
+                <p>
+                    Sites vitrine, landing pages et intégrations sur-mesure. <br />
+                    Disponible à distance, partout dans le monde.
+                </p>
+                <div>
+                    <a href="#">Voir mes offres</a>
+                    <a href="#">Discutons ↗</a>
+                </div>
+            </div>
+            <div>
+
+            </div>
+
+        </section>
+    )
+
+};
+
+export default Hero; 
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "/src/styles/_variables.scss" as *;`
+        additionalData: `
+          @use "/src/styles/_variables.scss" as *;
+          @use "/src/styles/_animations.scss" as *;
+        `
       }
     }
   }
