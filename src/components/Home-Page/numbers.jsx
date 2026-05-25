4f4b37a8c282e9986783ad5730a28345c9c6c0ae
Antoine Gadrat <antoinegadrat@hotmail.fr>
2026-05-25 18:32:13 +0900

    feat: forWho section

---
+++ src/components/Home-Page/numbers.jsx
@@ -0,0 +1,31 @@
+import '../../styles/components/Home_page/Numbers.scss'
+
+function Numbers() {
+    return(
+        <section className='sectionNumbers'>
+            <div className="container">
+                    <h2>En quelques chiffres</h2>
+                <div className="reassurance-flex">
+                    <div className="reass-card">
+                        <div className="reass-val">7j</div>
+                        <div className="reass-label">Délai livraison site une page</div>
+                    </div>
+                    <div className="reass-card">
+                        <div className="reass-val">24h</div>
+                        <div className="reass-label">Délai de réponse garanti</div>
+                    </div>
+                    <div className="reass-card">
+                        <div className="reass-val">100%</div>
+                        <div className="reass-label">Des fichiers remis au client</div>
+                    </div>
+                    <div className="reass-card">
+                        <div className="reass-val">0€</div>
+                        <div className="reass-label">Pour un devis détaillé</div>
+                    </div>
+                </div>
+            </div>
+</section>
+
+    );
+}
+export default Numbers;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Hero from "../components/Hero-home";
 import DispoBand from "../components/Dispo-band";
+import Numbers from "../components/Home-Page/numbers";
+import ForWho from "../components/Home-Page/ForWhoSection";
 
 function Home() {
   return (
     <>
       <Hero />
       <DispoBand />
+      <Numbers />
+      <ForWho />
     </>
   );
 }
